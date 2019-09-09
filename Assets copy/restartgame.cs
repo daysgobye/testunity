640d2987455660d5ac289c03d2ea8ef571d81054
Cole Smith <coles@Coles-MacBook-Pro.local>
2019-09-08 23:33:26 -0700

    test first commit

---
+++ Assets copy/restartgame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class restartgame : MonoBehaviour
+{
+    public GameObject startPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            Debug.Log("in herc");
+            gameObject.transform.position = startPoint.transform.position;
+        }
+    }
+}
---
+++ Assets copy/scripts/playerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontalMove = 0;
+    public float moveSpeed = 40;
+    bool jump = false;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false,jump);
+        jump = false;
+    }
+}
